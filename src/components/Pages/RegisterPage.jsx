79c4d842258b3d27f2fe18f3b9b2c20a0dc40571
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-12-26 14:50:19 +0100

    Create RegisterPage.jsx

---
+++ src/components/Pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from '../RegiserForm/RegisterForm';
+import React from 'react';
+
+export default function RegisterPage() {
+
+  return (
+    <div className='container'>
+          <h1>Register Page</h1>
+          <RegisterForm />
+    </div>
+  )
+}
