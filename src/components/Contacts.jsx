f0b2fd3275c14a171d354c3d8651b6e39fdb9769
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-12-29 00:30:19 +0100

    Create Contacts.jsx

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from "./Form/Form";
+import { Filter } from './Filter/Filter';
+import { Loader } from "./Loader/Loader";
+import { ContactList } from './ContactList/ContactList';
+import './Form/form.css';
+import { useEffect } from "react";
+import { fetchContacts } from "redux/contacts/contacts-operations";
+import { useDispatch, useSelector } from "react-redux";
+import { getFilteredContacts, selectIsLoading, selectError } from "redux/contacts/contacts-selector";
+
+const Contacts = () => {
+
+  const contacts = useSelector(getFilteredContacts);
+  
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+ 
+  return (
+      <>
+      <div className="form_wrapper">
+        <h1>Phonebook</h1>
+        <Form />
+        
+      </div> 
+      <div className="phonebook_contaner">
+        <h2> Contacts</h2> 
+        <Filter />
+         {/* <ContactList className="phonebook-container" />  */}
+        {isLoading && !error && <Loader />}
+        {contacts?.length > 0 && (<ContactList className="phonebook-container" />  )}
+        {/* <ContactList /> */}
+      </div>
+     {error && <p>Oooops... Something went wrong</p>}
+      </>
+    )
+}
+
+export default Contacts;
+
+// {contacts?.lenght === 0 && !isLoading && (<p>Contacts list is empty! Try to add contact</p>)} 
+//         {contacts?.lenght === 0 && (<p>Contacts list is empty! Try to add contact</p>)}
+
