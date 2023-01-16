a93f07ef30bd1292fe2ec5eb4527cf4c1d1481a1
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-01-16 11:15:46 +0100

    Update contacts-slice.js

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -17,30 +17,32 @@ const contactsSlice = createSlice({
     isLoading: false,
     error: null,
   },
-       extraReducers: {
-            [fetchContacts.pending]: handlePending,
-            [fetchContacts.fulfilled](state, action) {
-              state.isLoading = false;
-              state.error = null;
-              state.items = action.payload;
-            },
-            [fetchContacts.rejected]: handleRejected,
-            [addContact.pending]: handlePending,
-            [addContact.fulfilled](state, action) {
-              state.isLoading = false;
-              state.error = null;
-              state.items.push(action.payload);
-            },
-            [addContact.rejected]: handleRejected,
-            [deleteContact.pending]: handlePending,
-            [deleteContact.fulfilled](state, action) {
-              state.isLoading = false;
-              state.error = null;
-              state.items = state.items.filter(
-                contact => contact.id !== action.payload.id
-              );
-            },
-        },
-});  
+     extraReducers: {
+          [fetchContacts.pending]: handlePending,
+          [fetchContacts.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items = action.payload;
+          },
+          [fetchContacts.rejected]: handleRejected,
+          [addContact.pending]: handlePending,
+          [addContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items.push(action.payload);
+          },
+          [addContact.rejected]: handleRejected,
+          [deleteContact.pending]: handlePending,
+          [deleteContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items = state.items.filter(
+              contact => contact.id !== action.payload.id
+            );
+          },
+      },
+  }
+  );  
 
-export const contactsReducer = contactsSlice.reducer;
+
+  export const contactsReducer = contactsSlice.reducer;
